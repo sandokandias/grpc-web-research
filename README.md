2cacc233d52ef51ea1ff0cd5cea11225c9bdee1e
Sandokan <sandokandias@gmail.com>
2021-03-05 19:24:02 -0300

    fix k8s-manifests, frontend

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # grpc-consumer-research
-P&amp;D sobre abordagens de como lidar com clientes http tradicionais consumindo serviços grpc
+R&amp;D about how to consume grpc services by traditional http clients running on browser.
+
+
+## Dependency tools that you need to nstall:
+ - docker (https://www.docker.com/)
+ - k3d (https://k3d.io/)
+ - istioctl (https://istio.io/latest/docs/setup/getting-started/#download)
+  
+
+## Cluster setup
+1. Create the k3d cluster
+k3d cluster create my-multinode-cluster \
+    --servers 1 --agents 3 --port 9080:80@loadbalancer \
+    --port 9443:443@loadbalancer --api-port 6443 --k3s-server-arg '--no-deploy=traefik'
+
+2. Install istio on cluster
+istioctl install --set profile=default
+
+3. Enable automatic istio injection
+kubectl label namespace default istio-injection=enabled
+
+## Option 1: grpc-web
+3. Deploy backend with istio inject
+kubectl apply -f <(istioctl kube-inject -f backend.yml)
+
+4. Create istio gateway
+kubectl apply -f istio-gateway.yml
+
+5. Deploys frontend
+kubectl apply -f frontend.yml
--- frontend/client.js
+++ frontend/client.js
@@ -3,14 +3,30 @@ const {TimeServiceClient} = require('./grpc/api/time_grpc_web_pb.js');
 
 var client = new TimeServiceClient('http://' + window.location.host);
 
-var lbunix = document.getElementById('lbunix');
-var lbutc = document.getElementById('lbutc');
+var table = document.querySelector("table");
+while(table.hasChildNodes()) {
+  table.removeChild(table.firstChild)
+}
 
 window.getTime = function() {
   var request = new TimeRequest();
 
-  client.get_time(request, {}, (err, response) => {
-    lbunix.innerText = response.getUnix();
-    lbutc.innerText = response.getUtc();
+  client.get_time(request, {}, (err, response) => {    
+    if (err) {
+      var lb = document.getElementById("error")
+      lb.innerText = err
+    } else {
+      var header = table.insertRow(0);
+      var h1 = header.insertCell(0);
+      var h2 = header.insertCell(1);
+      h1.innerHTML = "UNIX";
+      h2.innerHTML = "UTC";
+
+      var row = table.insertRow(1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = response.getUnix();
+      cell2.innerHTML = response.getUtc();
+    }
   });
 };
