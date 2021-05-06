0446461b11911cc6d344af321d5a9182ef290c8c
Antonio Oliveira <antonio.filho@zup.com.br>
2021-05-06 09:42:19 -0300

    gitignore reset

---
+++ backend-kotlin/build/tmp/kapt3/stubs/main/grpc/web/research/ApplicationKt.java
@@ -0,0 +1,11 @@
+package grpc.web.research;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\u0014\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u0011\n\u0002\u0010\u000e\n\u0002\b\u0002\u001a\u0019\u0010\u0000\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003\u00a2\u0006\u0002\u0010\u0005\u00a8\u0006\u0006"}, d2 = {"main", "", "args", "", "", "([Ljava/lang/String;)V", "grpc-web-research"})
+public final class ApplicationKt {
+    
+    public static final void main(@org.jetbrains.annotations.NotNull()
+    java.lang.String[] args) {
+    }
+}
